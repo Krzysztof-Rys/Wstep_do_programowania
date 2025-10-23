84dec309e43ac1a33fb7128026f414916ad8282d
Krzysztof-Rys <73765@student.wsiz.edu.pl>
2025-10-24 00:39:32 +0200

    Add files via upload

---
+++ laboratory1/zadanie4.py
@@ -0,0 +1,6 @@
+from tkinter.constants import ROUND
+
+Cena = 39.99
+Rabat = 0.2
+porabacie = Cena * (1 - Rabat)
+print('Po naliczeniu rabatu:',round(porabacie,2),'zł')
---
+++ laboratory1/zadnie5.py
@@ -0,0 +1,8 @@
+
+print('OBLICZ POLE I OBWÓD PROSTOKĄTA \n ')
+y = float(input("Podaj długość y:"))
+x = float(input('Podaj długość x:'))
+pole = y * x
+obwod = 2*x + 2*y
+print(f'Pole prostokąta: {pole} cm^2\nObwód prostokąta: {obwod} cm.')
+
