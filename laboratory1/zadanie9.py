b8053cfcf13dfa9507f0a12ad4b0343076020d90
Krzysztof-Rys <73765@student.wsiz.edu.pl>
2025-10-24 21:24:41 +0200

    Delete laboratory1 directory

--- laboratory1/zadanie9.py
+++
@@ -1,9 +0,0 @@
-print("        KALKULKATOR \n##############################")
-x = float(input("Podaj pierwszą liczbę:"))
-y = float(input("Podaj drugą liczbę:"))
-dodawanie = x+y
-odejmowanie = x-y
-mnozenie = x*y
-dzielenie = x/y
-potegowanie = x**y
-print(f'Dodwanie: {dodawanie}\nOdejmowanie: {odejmowanie}\nDzielenie: {dzielenie}\nPotegowanie: {potegowanie}')
